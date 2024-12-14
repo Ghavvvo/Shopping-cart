1fcbc798afb9f90cb3196bf8c5f0f7fbaa292c62
gabrielherreca <2gaherre12@gmail.com>
2024-12-13 23:38:27 -0500

    Creating product type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import {Products} from "./components/Products.tsx";
 import {products} from "./mocks/products.json";
+import {useState} from "react";
+import {Product} from "./types.tsx";
+
 function App() {
+    const [filters, setFilters] = useState(
+        {
+            category: 'all',
+            minPrice: 0
+        }
+    )
+
+    const filterProducts = (products : Product[]) => {
+        return products.filter((product : Product ) => {
+            return (
+                product.price >= filters.minPrice &&
+                (filters.category === 'all' || product.category === filters.category)
+            )
+        })
+    }
+
     return (
-        <div >
-           <Products products={products}></Products>
+        <div>
+            <Products products={products}></Products>
         </div>
     )
 }
